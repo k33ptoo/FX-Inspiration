b7b4d3f1f0c82b420067168e9563490a08890460
KeepToo <akchieng@gmail.com>
2018-05-22 20:35:59 +0200

    Added project files

---
+++ src/fx_inspiration/HomeController.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fx_inspiration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.StackedAreaChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author Admin
+ */
+public class HomeController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+     @FXML
+    private VBox pnl_scroll;
+     
+       @FXML
+    private BarChart bar_chart;
+       
+           @FXML
+    private StackedAreaChart area_chart;
+    
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+       
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+        Node [] nodes = new  Node[15];
+        
+        for(int i = 0; i<15; i++)
+        {
+            try {
+                nodes[i] = (Node)FXMLLoader.load(getClass().getResource("Item.fxml"));
+                pnl_scroll.getChildren().add(nodes[i]);
+                
+            } catch (IOException ex) {
+                Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+           
+        }  
+        plotBarChart();
+        plotChart();
+        
+    }
+    
+    
+    
+    
+    private void plotChart()
+    {
+        XYChart.Series seriesApril= new XYChart.Series();
+        seriesApril.setName("April");
+        seriesApril.getData().add(new XYChart.Data(1, 4));
+        seriesApril.getData().add(new XYChart.Data(3, 10));
+        seriesApril.getData().add(new XYChart.Data(6, 15));
+        seriesApril.getData().add(new XYChart.Data(9, 8));
+        seriesApril.getData().add(new XYChart.Data(12, 5));
+        seriesApril.getData().add(new XYChart.Data(15, 18));
+        seriesApril.getData().add(new XYChart.Data(18, 15));
+        seriesApril.getData().add(new XYChart.Data(21, 13));
+        seriesApril.getData().add(new XYChart.Data(24, 19));
+        seriesApril.getData().add(new XYChart.Data(27, 21));
+        seriesApril.getData().add(new XYChart.Data(30, 21));
+        
+        XYChart.Series seriesMay = new XYChart.Series();
+        seriesMay.setName("May");
+        seriesMay.getData().add(new XYChart.Data(1, 20));
+        seriesMay.getData().add(new XYChart.Data(3, 15));
+        seriesMay.getData().add(new XYChart.Data(6, 13));
+        seriesMay.getData().add(new XYChart.Data(9, 12));
+        seriesMay.getData().add(new XYChart.Data(12, 14));
+        seriesMay.getData().add(new XYChart.Data(15, 18));
+        seriesMay.getData().add(new XYChart.Data(18, 25));
+        seriesMay.getData().add(new XYChart.Data(21, 25));
+        seriesMay.getData().add(new XYChart.Data(24, 23));
+        seriesMay.getData().add(new XYChart.Data(27, 26));
+        seriesMay.getData().add(new XYChart.Data(31, 26));
+        
+        area_chart.getData().addAll(seriesApril, seriesMay);
+    }
+    
+    private void plotBarChart()
+    {
+        
+        String austria = "Austria";
+        String brazil = "Brazil";
+        String france = "France";
+        String italy = "Italy";
+        String usa = "USA";
+        
+         XYChart.Series series1 = new XYChart.Series();
+        series1.setName("2015");       
+        series1.getData().add(new XYChart.Data(austria, 25601.34));
+        series1.getData().add(new XYChart.Data(brazil, 20148.82));
+        series1.getData().add(new XYChart.Data(france, 10000));
+        series1.getData().add(new XYChart.Data(italy, 35407.15));
+        series1.getData().add(new XYChart.Data(usa, 12000));      
+        
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("2016");
+        series2.getData().add(new XYChart.Data(austria, 57401.85));
+        series2.getData().add(new XYChart.Data(brazil, 41941.19));
+        series2.getData().add(new XYChart.Data(france, 45263.37));
+        series2.getData().add(new XYChart.Data(italy, 117320.16));
+        series2.getData().add(new XYChart.Data(usa, 14845.27));  
+        
+        XYChart.Series series3 = new XYChart.Series();
+        series3.setName("2017");
+        series3.getData().add(new XYChart.Data(austria, 45000.65));
+        series3.getData().add(new XYChart.Data(brazil, 44835.76));
+        series3.getData().add(new XYChart.Data(france, 18722.18));
+        series3.getData().add(new XYChart.Data(italy, 17557.31));
+        series3.getData().add(new XYChart.Data(usa, 92633.68));  
+        
+        bar_chart.getData().addAll(series1, series2, series3);
+        
+    }
+    
+}
